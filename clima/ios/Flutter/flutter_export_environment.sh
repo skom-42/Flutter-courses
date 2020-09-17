f3c725aaa7069ad00646fa0a1765d8cf6f9267b0
skom-42 <pskomartsov@mail.ru>
2020-09-17 09:24:52 +0300

    add: get dependences

--- clima/ios/Flutter/flutter_export_environment.sh
+++ clima/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/skom_42/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/skom_42/Documents/Studying/Flutter_courses/FlutterPractice/clima"
-export "FLUTTER_TARGET=/Users/skom_42/Documents/Studying/Flutter_courses/FlutterPractice/clima/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/skom_42/Documents/Studying/Flutter-courses/clima"
+export "FLUTTER_TARGET=/Users/skom_42/Documents/Studying/Flutter-courses/clima/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
