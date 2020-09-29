3514cff3815642f6be10244d555c72c273e39770
skom-42 <pskomartsov@mail.ru>
2020-09-29 08:18:38 +0300

    add: todoey ui

---
+++ todoey_flutter/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:todoey_flutter/screens/tasks_screens.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TasksScreen(),
+    );
+  }
+}
